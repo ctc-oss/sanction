e1829c7d9f43f2d68212c076e17a8504ae2e3a9d
John Wass <wassj@ctc.com>
2021-02-04 20:29:34 -0500

    wip pretty print

--- src/markdown.rs
+++ src/markdown.rs
@@ -1,14 +1,22 @@
 use crate::vuln::Match;
+use termimad::*;
+use crossterm::style::Color::*;
+
+static HEADER: &str = r#"
+|:-:|:--------:|---------|-----|-----
+|   | category | package | cve | fix |
+|---|----------|---------|-----|-----"#;
 
 pub fn dump_table(matches: Vec<&Match>, allowlist: Vec<String>) {
-    println!("| A | category | package | cve | fix |");
-    println!("|---|----------|---------|-----|-----|");
+    let mut txt: Vec<String> = vec![];
+    txt.push(HEADER.to_string());
+
     let empty = String::new();
     for m in matches.iter() {
         let f = &m.vulnerability;
         let fix = f.fixed_in_version.as_ref().unwrap_or(&empty);
         let wl = if allowlist.contains(&m.vulnerability.id) {
-            "X"
+            "*X*"
         } else {
             ""
         };
@@ -16,9 +24,20 @@ pub fn dump_table(matches: Vec<&Match>, allowlist: Vec<String>) {
             Some(l) => format!("[{}]({})", f.id, l),
             None => "N/A".to_string(),
         };
-        println!(
-            "| {} | {} | {} | {} | {} |",
+        let row = format!(
+            "| ** {} ** | ~~ {} ~~ | {} | {} | {} |",
             wl, f.severity, m.artifact, cve, fix
         );
+        txt.push(row);
     }
+
+    txt.push("|-".to_string());
+    let full: String = txt.join("\n");
+
+    let mut skin = MadSkin::default();
+    skin.set_headers_fg(rgb(255, 187, 0));
+    skin.bold.set_fg(Yellow);
+    skin.paragraph.align = Alignment::Center;
+    skin.table.align = Alignment::Left;
+    println!("{}", skin.term_text(&full));
 }
